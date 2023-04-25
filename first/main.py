537916542a82bcd60f2dc22cf249782064515436
jack.wei <jackwgs@126.com>
2023-04-26 00:04:17 +0800

    re

--- first/main.py
+++ first/main.py
@@ -1,12 +1,13 @@
 # This is a sample Python script.
 import requests
+import re
 
 
 def get(data):
     url = f"https://www.sogou.com/web?query={data}"
     headers = {
-    "User-Agent":
-    "Mozilla / 5.0(Macintosh;\
+        "User-Agent":
+            "Mozilla / 5.0(Macintosh;\
        Intel\
        Mac\
        OS\
@@ -16,7 +17,7 @@ def get(data):
        .0\
        .0\
       Safari / 537.36"
-     }
+    }
     resp = requests.get(url, headers=headers)
     resp.encoding = "utf-8"
     with open("sougou.html", mode="w", encoding="utf-8") as f:
@@ -33,15 +34,15 @@ def post(data):
 def movie():
     url = "https://movie.douban.com/j/chart/top_list"
     data = {
-    "type": "13",
-      "interval_id": "100:90",
-      "action":"",
-      "start": 0,
-      "limit": 20
-     }
+        "type": "13",
+        "interval_id": "100:90",
+        "action": "",
+        "start": 0,
+        "limit": 20
+    }
     headers = {
-    "User-Agent":
-    "Mozilla / 5.0(Macintosh;\
+        "User-Agent":
+            "Mozilla / 5.0(Macintosh;\
        Intel\
        Mac\
        OS\
@@ -51,12 +52,37 @@ def movie():
        .0\
        .0\
       Safari / 537.36"
-     }
+    }
     resp = requests.get(url, params=data, headers=headers)
     print(resp.json())
 
 
+def test_re():
+    # r 防止转义字符
+    result1 = re.findall(r".*?x", "aaaxbbbx")
+    print("findall", result1)
+
+    # 迭代器
+    result2 = re.finditer(r".*?x", "aaaxbbbx")
+    for item in result2:
+        print(item.group())
+
+    # 只会匹配到第一次匹配的内容
+    result3 = re.search(r".*?x", "aaaxbbbx")
+    print("search", result3.group())
+
+    # match 匹配的时候从字符串的开头开始匹配 类似在正则前面加上^
+    result3 = re.match(r".*?x", "aaaxbbbx")
+    print("match", result3.group())
+    # 预加载 提前把正则对象加载完毕
+    obj = re.compile(r".*?x")
+    # 直接把编译好的正则进行使用
+    result4 = obj.findall("aaaxbbbx")
+    print("compile", result4)
+
+
 if __name__ == '__main__':
     # content = input("请输入一个单词: ")
-     # post(content)
-     movie()
+    # post(content)
+    # movie()
+    test_re()
